654da983fc9b15e01da9a16618914f3bffa8372e
John Olsson <john@cryon.se>
2016-10-08 08:03:34 +0200

    Add dependency on Emacs 24

--- octo-mode.el
+++ octo-mode.el
@@ -7,6 +7,7 @@
 ;; URL: https://github.com/cryon/octo-mode
 ;; Created: 4th October 2016
 ;; Version: 0.1.0
+;; Package-Requires: ((emacs "24"))
 ;; Keywords: languages
 
 ;; This file is free software: you can redistribute it and/or modify
